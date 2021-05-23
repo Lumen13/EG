1e5f62baa539b29fe38aa5982a55865cc139178f
Lumen13 <ntap13@gmail.com>
2021-05-23 19:22:21 +0300

    Проект EG (Eagle Games) стартовал!
    Синхронизация с GitHub. Стек - .net core 3.1 + Angular 10. Ориентировочная архитектура солюшна на данный момент разбита на 3 проекта: дата, EF и Docking. Дата будет содержать информацию о товарах, моделях и прочих сущностях с которыми предстоит работать. EF - проект Entity Framework, который будет абстрагировать фреймворк от других частей проекта ради взаимозаменяемости данного модуля, что считаю целесообразным. Docking - проект запускатор, по умолчанию дефолтный и создающий мастер-соединение. На данный момент выступает так же в роли прослойки между backend и frontend частями проекта. Проект Docking, так же, включает в себя Node Modules и сам Angular. В будущем необходимо будет абстрагировать и Angular пересмотрев архитектуру проекта. Сейчас солюшн реализует базовые CRUD операции, которые можно наглядно прокликать на примере работы с товарами в стиле блокнота-памятки.
    
    The EG (Eagle Games) project has started!
    GitHub synchronization. The technology-stack is .NET Core 3.1 + Angular 10. The approximate architecture of solution is currently divided into 3 projects: Date, EF and Docking. The date will contain information about the products, models, and other entities with which the interaction will take place. EF is an Entity Framework project that will abstract this framework from other parts of the project for the sake of interchangeability of this module, which is appropriate, I think. Docking is the default project-initiator, that creates a master connection. At the moment, it also acts as a layer between the backend and frontend parts of EG solution. The Docking project also includes Node Modules and Angular itself. It will be necessary to abstract and Angular by revising the project architecture in future. Now solution implements basic CRUD operations, which can be clearly displayed on the example of working with products in the style of a memo pad

---
+++ Data.EF/Models/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Data.Models;
+
+namespace Data.EF.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
